cc6a9ce7ce9e5168905dc6402e3f2140346f54b0
Karina0608 <karinach03@yandex.ru>
2022-11-12 02:16:46 +0300

    Практическая работа 1: классы, как новые типы данных. Поля данных и методы.

---
+++ src/pract_1/Book.java
@@ -0,0 +1,35 @@
+package pract_1;
+import java.lang.*;
+
+public class Book {
+    private String name;
+    private int publ_date;
+
+    public Book(String n, int year){
+        name = n;
+        publ_date = year;
+    }
+    public Book(String n){
+        name = n;
+        publ_date = 0;
+    }
+    public Book(){
+        name = "Алиса в стране чудес";
+        publ_date = 2020;
+    }
+    public void setDate (int publ_date){
+        this.publ_date = publ_date;
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+    public int getDate(){
+        return publ_date;
+    }
+    public String toString(){
+        return this.name+", "+this.publ_date;
+    }
+    public void library(){
+        System.out.println("Название: " + name + ", Год: " + publ_date);
+    }
+}
---
+++ src/pract_1/Main.java
@@ -0,0 +1,13 @@
+package pract_1;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Остров сокровищ", 2012);
+        Book book2 = new Book("Кот в сапогах");
+        book2.setDate(2000);
+        System.out.println(book1);
+        book1.library();
+        book2.library();
+    }
+}
