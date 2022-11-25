d38f1a052aa7e50ee00f3f61d318aa0ac9b19c6f
Karina0608 <karinach03@yandex.ru>
2022-11-26 00:52:13 +0300

    Лабораторная работа 3: Наследование в java

---
+++ src/lab_3/Main.java
@@ -0,0 +1,8 @@
+package lab_3;
+
+public class Main {
+    public static void main(String[] args){
+        Cup cup1 = new Cup("стекло", "белый", "300 мл");
+        System.out.println(cup1);
+    }
+}
