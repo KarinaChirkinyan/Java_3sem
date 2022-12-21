5ca3706423251a89612159982019315aa5a8bda9
Karina0608 <karinach03@yandex.ru>
2022-12-21 10:12:47 +0300

    java

--- src/lab_5/MyApp.java
+++ src/lab_5/MyApp.java
@@ -1,43 +1,35 @@
 package lab_5;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.io.File;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
 
-// App class
 class MyApp extends JFrame {
-    int WINDOW_WIDTH = 600;
-    int WINDOW_HEIGHT = 600;
-    String background_image_path;
+    private JLabel imgLabel;
+    private int imgNum = 1;
 
-    int method;
+    private MyApp(){
+        this.setSize(1200, 1200);
+        imgLabel = new JLabel("", new ImageIcon(getClass().getResource("Perlamutrovy.png")), SwingConstants.CENTER);
 
-    // Init
-    MyApp() {
-        super("Some shapes");
-        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        setLocation(300, 300);
-        setLayout(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        getContentPane().add(imgLabel);
 
-        this.method = 0;
+        //Timer timer = new Timer(2000, e -> nextImg());
+        //timer.start();
+    }
+    private void nextImg(){
+        imgLabel.setIcon(new ImageIcon(getClass().getResource((++imgNum)+".jpeg")));
+        if(imgNum == 5) imgNum = 0;
+
     }
 
-    void set_background_image_path(String path) {
-        this.background_image_path = path;
-    }
-
-
-    // Start app
     public static void main(String[] args) {
-        MyApp app = new MyApp();
-        if (args.length == 1) {
-            app.set_background_image_path(args[0]);
-        }
-        else {
-            System.out.println("Background image path is empty");
-        }
+        MyApp mf = new MyApp();
+        mf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mf.setVisible(true);
+
     }
 }
 
